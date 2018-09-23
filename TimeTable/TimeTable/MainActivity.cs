4dd6f4287ba7d9260494bb97549114282ec9e731
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-09-23 19:58:43 +1200

    Button

--- TimeTable/TimeTable/MainActivity.cs
+++ TimeTable/TimeTable/MainActivity.cs
@@ -1,7 +1,6 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
 using Android.Widget;
 
 namespace TimeTable
@@ -12,9 +11,14 @@ namespace TimeTable
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
+            var btnMonday = FindViewById<Button>(Resource.Id.btnMonday);
+            btnMonday.Click += BtnMonday_Click;
+        }
+        private void BtnMonday_Click(object sender, System.EventArgs e)
+        {
+            FindViewById<TextView>(Resource.Id.MonClass).Text = "8am-10am | Interface Analysis & Design | 10am-12pm | Programming";
         }
 
     }
