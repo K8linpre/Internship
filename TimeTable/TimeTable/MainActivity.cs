a0a165ab2cac4d84024cccfde53f275432cd35cb
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-09-24 17:59:59 +1200

    button

--- TimeTable/TimeTable/MainActivity.cs
+++ TimeTable/TimeTable/MainActivity.cs
@@ -11,7 +11,7 @@ namespace TimeTable
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.layout.activity_main);
 
             var btnMonday = FindViewById<Button>(Resource.Id.btnMonday);
             btnMonday.Click += BtnMonday_Click;
@@ -20,6 +20,5 @@ namespace TimeTable
         {
             FindViewById<TextView>(Resource.Id.MonClass).Text = "8am-10am | Interface Analysis & Design | 10am-12pm | Programming";
         }
-
     }
 }
